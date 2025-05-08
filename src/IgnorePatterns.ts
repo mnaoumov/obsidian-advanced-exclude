f84ad0247c8f00f2a22a468c03a60d295eb5c9c9
Michael Naumov <mnaoumov@gmail.com>
2025-05-08 09:00:57 -0600

    Extract DataAdapterSafe

--- src/IgnorePatterns.ts
+++ src/IgnorePatterns.ts
@@ -1,16 +1,15 @@
-import type {
-  App,
-  Stat
-} from 'obsidian';
+import type { App } from 'obsidian';
 
 import ignore from 'ignore';
-import {
-  CapacitorAdapter,
-  FileSystemAdapter
-} from 'obsidian';
 import { escapeRegExp } from 'obsidian-dev-utils/RegExp';
 
 import type { Plugin } from './Plugin.ts';
+
+import {
+  readSafe,
+  statSafe,
+  writeSafe
+} from './DataAdapterSafe.ts';
 
 export const ROOT_PATH = '/';
 export const OBSIDIAN_IGNORE_FILE = '.obsidianignore';
@@ -60,21 +59,6 @@ export async function isIgnored(normalizedPath: string, plugin: Plugin, isFolder
 
 export async function setIgnorePatternsStr(app: App, ignorePatterns: string): Promise<void> {
   await writeSafe(app, OBSIDIAN_IGNORE_FILE, ignorePatterns);
-}
-
-async function existsSafe(app: App, path: string): Promise<boolean> {
-  const adapter = app.vault.adapter;
-  if (adapter instanceof FileSystemAdapter) {
-    const fullPath = adapter.getFullPath(path);
-    try {
-      await adapter.fsPromises.access(fullPath);
-      return true;
-    } catch {
-      return false;
-    }
-  }
-
-  return await adapter.exists(path);
 }
 
 async function getAllIgnorePatternsStr(plugin: Plugin): Promise<string> {
@@ -136,63 +120,4 @@ async function getIgnoreTester(plugin: Plugin): Promise<ignore.Ignore> {
     ignoreCase: true
   }).add(ignorePatternsStr.split('\n'));
   return cachedIgnoreTester;
-}
-
-async function readSafe(app: App, path: string): Promise<string> {
-  if (!await existsSafe(app, path)) {
-    return '';
-  }
-
-  const adapter = app.vault.adapter;
-  const fullPath = adapter.getFullPath(path);
-
-  if (adapter instanceof FileSystemAdapter) {
-    return await adapter.fsPromises.readFile(fullPath, 'utf8');
-  }
-  if (adapter instanceof CapacitorAdapter) {
-    return await adapter.fs.read(fullPath);
-  }
-
-  throw new Error('Unknown adapter');
-}
-
-async function statSafe(app: App, path: string): Promise<null | Stat> {
-  const adapter = app.vault.adapter;
-  const fullPath = adapter.getFullPath(path);
-  if (adapter instanceof FileSystemAdapter) {
-    const fsStats = await adapter.fsPromises.stat(fullPath);
-    return {
-      ctime: Math.round(fsStats.birthtimeMs),
-      mtime: Math.round(fsStats.mtimeMs),
-      size: fsStats.size,
-      type: fsStats.isFile() ? 'file' : 'directory'
-    } as Stat;
-  }
-  if (adapter instanceof CapacitorAdapter) {
-    const fsStats = await adapter.fs.stat(fullPath);
-    return {
-      ctime: fsStats.ctime ?? 0,
-      mtime: fsStats.mtime ?? 0,
-      size: fsStats.size ?? 0,
-      type: fsStats.type
-    } as Stat;
-  }
-  throw new Error('Unknown adapter');
-}
-
-async function writeSafe(app: App, path: string, content: string): Promise<void> {
-  const adapter = app.vault.adapter;
-  const fullPath = adapter.getFullPath(path);
-
-  if (adapter instanceof FileSystemAdapter) {
-    await adapter.fsPromises.writeFile(fullPath, content);
-    return;
-  }
-
-  if (adapter instanceof CapacitorAdapter) {
-    await adapter.fs.write(fullPath, content);
-    return;
-  }
-
-  throw new Error('Unknown adapter');
 }
