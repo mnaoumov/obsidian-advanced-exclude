4ee61b8127b185ffe0f32e7afcb4428c2a48542e
Michael Naumov <mnaoumov@gmail.com>
2026-04-08 08:12:57 -0600

    chore: remove unused test scripts

--- scripts/test-coverage.ts
+++
@@ -1,8 +0,0 @@
-import { wrapCliTask } from 'obsidian-dev-utils/script-utils/cli-utils';
-import { testCoverage } from 'obsidian-dev-utils/script-utils/test-runners/vitest';
-
-await wrapCliTask(() =>
-  testCoverage({
-    projects: ['unit-tests']
-  })
-);
--- scripts/test-watch.ts
+++
@@ -1,8 +0,0 @@
-import { wrapCliTask } from 'obsidian-dev-utils/script-utils/cli-utils';
-import { testWatch } from 'obsidian-dev-utils/script-utils/test-runners/vitest';
-
-await wrapCliTask(() =>
-  testWatch({
-    projects: ['unit-tests']
-  })
-);
